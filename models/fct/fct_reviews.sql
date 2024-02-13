b9096bd9a4873f07b01c3b6a9cd6abd9c41ecb85
Neha yerragudi <neha.s.yerragudi@kipi.bi>
2024-02-13 10:32:38 +0000

    fact commit

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -1,5 +1,14 @@
-{{ config(materialized="table") }}
-with src_reviews as (select * from {{ ref("src_reviews") }})
-select *
-from src_reviews
-where review_text is not null
+{{
+ config(
+ materialized = 'incremental',
+ on_schema_change='fail'
+ )
+}}
+WITH src_reviews AS (
+ SELECT * FROM {{ ref('src_reviews') }}
+)
+SELECT * FROM src_reviews
+WHERE review_text is not null
+{% if is_incremental() %}
+ AND review_date > (select max(review_date) from {{ this }})
+{% endif %}
