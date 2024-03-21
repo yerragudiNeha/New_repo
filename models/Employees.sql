95199762ef4900faa04446ddc7f5904211fc2147
Neha yerragudi <neha.s.yerragudi@kipi.bi>
2024-03-21 03:53:51 +0000

    emp commit

--- models/Employees.sql
+++ models/Employees.sql
@@ -1,6 +1,21 @@
 {{ 
    config(
    materialized='table'
-) }}
+) 
+}}
 
-{{ append_mode('raw', 'employee_details')}}
+with cte as(
+      select * from  {{source ('raw','employee_details')}}
+   ) 
+select employee_id,
+           employee_first_name,
+           employee_last_name,
+           department,
+           designation,
+           employee_address,
+           employee_email,
+           employee_phone_number,
+           last_modified_timestamp
+
+from cte
+
