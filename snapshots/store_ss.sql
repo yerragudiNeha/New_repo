dc0cfaefd81adfdc2483c9abb77c8916272b0a4e
Neha yerragudi <neha.s.yerragudi@kipi.bi>
2024-03-22 08:16:08 +0000

    store commit

--- snapshots/store_ss.sql
+++ snapshots/store_ss.sql
@@ -6,7 +6,7 @@
    target_schema='snapshot',
    unique_key='S_STORE_ID',
    strategy='check',
-   check_cols=['S_STORE_NAME','S_MANAGER','S_MARKET_MANAGER']
+   check_cols=['S_STORE_NAME','S_MARKET_ID']
 ) }}
 
 select * from {{ source('raw','store') }}
